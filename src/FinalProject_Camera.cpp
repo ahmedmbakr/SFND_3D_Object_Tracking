b0ca0c9a0cd9816fe00dc373dd2537de2d6a858e
Ahmed Bakr <ahmed.m.ali.bakr@gmail.com>
2020-01-07 01:32:36 -0800

    Change image size to 1000 X 1000 for the points to be viewed

--- src/FinalProject_Camera.cpp
+++ src/FinalProject_Camera.cpp
@@ -132,7 +132,8 @@ int main(int argc, const char *argv[])
         bVis = true;
         if(bVis)
         {
-            show3DObjects((dataBuffer.end()-1)->boundingBoxes, cv::Size(4.0, 20.0), cv::Size(2000, 2000), true);
+            bool waitUntilUserPressZero = true;
+            show3DObjects((dataBuffer.end()-1)->boundingBoxes, cv::Size(4.0, 20.0), cv::Size(1000, 1000), waitUntilUserPressZero);
         }
         bVis = false;
 
